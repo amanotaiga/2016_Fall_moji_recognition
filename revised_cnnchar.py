f0edb8562219e66e675f580db4f1da20f9786892
amanotaiga <amanotaiga3665@gmail.com>
2016-10-30 20:58:35 +0900

    epoch to 40

--- revised_cnnchar.py
+++ revised_cnnchar.py
@@ -153,5 +153,5 @@ m6_1()
 model.summary()
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 model.fit_generator(datagen.flow(X_train, Y_train, batch_size=16), samples_per_epoch=X_train.shape[0],
-                    nb_epoch=20, validation_data=(X_test, Y_test))	
+                    nb_epoch=40, validation_data=(X_test, Y_test))	
 
