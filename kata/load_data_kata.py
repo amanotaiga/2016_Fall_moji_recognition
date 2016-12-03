8831ab0232c32f7077268646daf1c6265959797f
amanotaiga <amanotaiga3665@gmail.com>
2016-12-03 19:28:14 +0900

    change folder

---
+++ kata/load_data_kata.py
@@ -0,0 +1,58 @@
+import struct
+import numpy as np
+from PIL import Image, ImageEnhance
+import numpy as np
+import h5py
+import tensorflow
+import scipy.misc
+
+
+sz_record = 2052
+
+def read_record_ETL6C(f):
+    s = f.read(2052)
+    r = struct.unpack('>H2sH6BI4H4B4x2016s4x', s)
+    iF = Image.frombytes('F', (64, 63), r[18], 'bit', 4)
+    iL = iF.convert('P')
+    enhancer = ImageEnhance.Brightness(iL)
+    iT = enhancer.enhance(20)
+    size_add = 12
+    iT = iT.resize((64 + size_add, 63+ size_add))
+    iT = iT.crop((size_add / 2,
+                  size_add / 2,
+                  64 + size_add / 2,
+                  63 + size_add / 2))
+    return r + (iT,)
+
+
+def get_ETL_data(filenum,img_print):
+        new_img = Image.new('1', (64, 64))
+        X = []
+        Y = []
+        filename = 'ETL6/ETL6C_{:02d}'.format(filenum)
+        with open(filename, 'rb') as f:
+            set_range = 10
+            f.seek(0 * 1383 * sz_record)
+            #8 different characters
+            if(filenum==5):
+                set_range = 8
+            for j in range(set_range):
+                for i in range(1383):
+                    r = read_record_ETL6C(f)
+                    new_img.paste(r[-1], (0, 0))
+                    iE = Image.eval(new_img, lambda x: not x)
+                    #resize image to 32,32
+                    iE.thumbnail((32,32))
+                    if(img_print==True):
+                        if(i%100==0):
+                           fn = 'ETL6C_ds{:02d}_{:02d} {:03d}.png'.format(filenum,j,i)
+                           iE.save('/mnt/d/Desktop/ETL6C_data/'+fn, 'PNG')
+                    shapes = 32, 32
+		    #put the image into an array
+                    outData = np.asarray(iE.getdata()).reshape(shapes[0], shapes[1])
+                    X.append(outData)	
+		    #append the label of the image
+                    Y.append(r[3])
+        X, Y = np.asarray(X, dtype=np.int32), np.asarray(Y, dtype=np.int32)
+		#return array X and array Y
+        return (X,Y)
--- kata/test_model_kata.py
+++ kata/test_model_kata.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import keras.callbacks
 import keras.backend.tensorflow_backend as KTF
 import matplotlib 
@@ -6,58 +7,39 @@ import matplotlib.pyplot as plt
 from keras.utils.visualize_util import plot
 from sklearn.metrics import classification_report,confusion_matrix
 from operator import itemgetter
+from keras.models import model_from_json
+import os
 
+import struct
 import numpy as np
+from PIL import Image, ImageEnhance
+import numpy as np
+import h5py
+from keras.utils import np_utils
+from sklearn import datasets, metrics, cross_validation
+from sklearn.utils import shuffle
+from keras.models import Sequential
+import tensorflow
+from tensorflow.python.ops import control_flow_ops 
+tensorflow.python.control_flow_ops = control_flow_ops
 import scipy.misc
 from keras import backend as K
 from keras import initializations
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
-import tensorflow
-from tensorflow.python.ops import control_flow_ops 
-tensorflow.python.control_flow_ops = control_flow_ops
+from convert_data_kata import data
 
-np.set_printoptions(threshold=np.nan)
 
-nb_classes = 50
-writer = 1383
-# input image dimensions
-img_rows, img_cols = 32, 32
-# img_rows, img_cols = 127, 128
-
-ary = np.load("ETL6C_kata_data.npz")['arr_0'].reshape([-1, 63, 64]).astype(np.float32) / 15
-X_train = np.zeros([nb_classes * writer, img_rows, img_cols], dtype=np.float32)
-for i in range(nb_classes * writer):
-    X_train[i] = scipy.misc.imresize(ary[i], (img_rows, img_cols), mode='F')
-    # X_train[i] = ary[i]
-Y_train = np.repeat(np.arange(nb_classes), writer)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.2)
+X_train, Y_train, X_test, Y_test,nb_classes,input_shape = data()
 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.25)
 
-if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    X_val = X_val.reshape(X_val.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    X_val = X_val.reshape(X_val.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+n_output = Y_train.shape[1]
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(Y_train, nb_classes)
-Y_test = np_utils.to_categorical(Y_test, nb_classes)
-Y_val  = np_utils.to_categorical(Y_val, nb_classes)
 
 datagen = ImageDataGenerator(rotation_range=15, zoom_range=0.20)
 datagen.fit(X_train)
-
 model = Sequential()
 
 
@@ -65,8 +47,6 @@ def my_init(shape, name=None):
     return initializations.normal(shape, scale=0.1, name=name)
 
 
-# Best val_loss: 0.0205 - val_acc: 0.9978 (just tried only once)
-# 30 minutes on Amazon EC2 g2.2xlarge (NVIDIA GRID K520)
 def m6_1():
     model.add(Convolution2D(32, 3, 3, init=my_init, input_shape=input_shape))
     model.add(Activation('relu'))
@@ -74,14 +54,12 @@ def m6_1():
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
-
     model.add(Convolution2D(64, 3, 3, init=my_init))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3, init=my_init))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
-
     model.add(Flatten())
     model.add(Dense(256, init=my_init))
     model.add(Activation('relu'))
@@ -95,16 +73,40 @@ def classic_neural():
     model.add(Dense(256))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
 
+def m6_2():
+    model.add(Convolution2D(32, 3, 3, init=my_init, input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, init=my_init))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(64, 3, 3, init=my_init))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3, init=my_init))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(128, 3, 3, init=my_init))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(512, init=my_init))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
 m6_1()
-# classic_neural()
+
 model.summary()
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 history = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=16), samples_per_epoch=X_train.shape[0],
-                     nb_epoch=40, validation_data=(X_test, Y_test))
+                    nb_epoch=13, validation_data=(X_val, Y_val))	
+
 
 print(history.history.keys())
 plt.plot(history.history['acc'])
@@ -114,7 +116,7 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'val'], loc='upper left')
 plt.show()
-plt.savefig('acc_kata.png')
+plt.savefig('acc_kata_M1.png')
 
 plt.clf()
 
@@ -125,7 +127,7 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'val'], loc='upper left')
 plt.show()
-plt.savefig('loss_kata.png')
+plt.savefig('loss_kata_M1.png')
 
 y_pred = model.predict_classes(X_test)
 #print(y_pred)
@@ -143,14 +145,20 @@ print('Test accuracy:', score[1])
 sorted_row_idx = np.argsort(confusion, axis=1)[:,confusion.shape[1]-3::]
 print(sorted_row_idx)
 
+map_character = ['ア','イ','ウ','エ','オ','カ','キ','ク','ケ','コ','サ','シ','ス','セ','ソ',
+'タ','チ','ツ','テ','ト','ナ','二','ヌ','ネ','ノ','ハ','ヒ','フ','ヘ','ホ',
+'マ','ミ','ム','メ','モ','ヤ','ユ','ヨ','ラ','リ','ル','レ','ロ','ワ','ヲ','ン',
+' d',' o',' l',' r']
 
-#num = -3  
-#top = np.argpartition(confusion, num, axis=1)[:, :num]
-#print(top)
-#confusion[np.arange(confusion.shape[0])[:, None], top]
-#print(confusion)
+for j in range(48):
+    print ()
+    for i in range(3):
+         key_map = sorted_row_idx[j,i]
+         print(map_character[key_map]),
 
-#nl = []
-#for i in confusion:
-#    nl.append(sorted(i,reverse=True))
-#print(np.matrix(nl))
+model_json = model.to_json()
+with open("model_kata.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model_kata.h5")
+print("Saved model to disk")
